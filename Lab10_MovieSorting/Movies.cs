0e1d27372e7affb70b1e08b62b641efcc4ae251b
Jonas <jkpaskus@gmail.com>
2017-04-24 14:24:25 -0400

    Initial commit of lab 10

---
+++ Lab10_MovieSorting/Movies.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10_MovieSorting
+{
+    class Movies
+    {
+        private string Category;
+        private string Title;
+
+        #region properties
+        public string MovieTitle
+        {
+            get
+            {
+                return Title;
+            }
+
+            set
+            {
+                Title = value;
+            }
+        }
+
+        public string MovieCategory
+        {
+            get
+            {
+                return Category;
+            }
+
+            set
+            {
+                Category = value;
+            }
+        }
+        #endregion
+
+        #region constructors
+        public Movies()
+        {
+            Category = "-----";
+            Title = "-----";
+        }
+
+        public Movies(string MTitle, string MCategory)
+        {  //this refers to global variable!! (class field)
+
+            Title = MTitle;
+            Category = MCategory;
+            
+        }
+
+        #endregion
+    }
+}
---
+++ Lab10_MovieSorting/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10_MovieSorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Movies[] ListOfMovies = new Movies[10];
+
+            ListOfMovies[0] = new Movies("Inception", "Scifi");
+            ListOfMovies[1] = new Movies("Scarface", "Drama");
+            ListOfMovies[2] = new Movies("Moana", "Animated");
+            ListOfMovies[3] = new Movies("Secret Life of Pets", "Animated");
+            ListOfMovies[4] = new Movies("Alien", "Horror");
+            ListOfMovies[5] = new Movies("Cujo", "Horror");
+            ListOfMovies[6] = new Movies("Carrie", "Horror");
+            ListOfMovies[7] = new Movies("Saving Private Ryan", "Drama");
+            ListOfMovies[8] = new Movies("Avatar", "Scifi");
+            ListOfMovies[9] = new Movies("Despicable Me", "Animated");
+
+            //Composition...entering values for movies, and cetegories
+            //for (int i = 0; i < ListOfMovies.Length; i++)
+            //{
+            //    if (ListOfMovies[i] != null)
+            //        Console.WriteLine($"\t{ListOfMovies[i].MovieCategory}\n\n\n");
+            //}
+
+            Console.Write("Please enter a category to search by: ");
+            string requestCategory = Console.ReadLine();
+            Console.WriteLine ($"You requested to search for {requestCategory} movies!!!");
+
+            foreach (Movies movie in ListOfMovies)
+            {
+                if ( requestCategory == movie.MovieCategory )
+                {
+                    Console.WriteLine($"This movie: {movie.MovieTitle}");
+                }
+                //else
+                //{
+                //    Console.WriteLine("Nope, TRY AGAIN!");
+                //}
+            }
+        }
+    }
+    }
+
